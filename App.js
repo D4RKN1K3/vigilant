1fb474e639c72d184b3ef53a39b60f344a878ac3
D4RKN1K3 <nicolas.barraza.cortes@alumnos.uta.cl>
2023-04-01 16:32:43 -0300

    Update App.js
    Probando cositas 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Holaaaa</Text>
+      <Text>Holaaaa como estas?</Text>
       <StatusBar style="auto" />
     </View>
   );
