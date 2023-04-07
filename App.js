e6d9213f7fe15bd9fa41bd6f0a847e36fbd30515
Bryan Alexander Godoy Aran <elmaildebryang@gmail.com>
2023-04-07 00:03:55 -0400

    Navigation mal
    estoy probando cosas

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View , Image,Button, Alert, Pressable } from 'react-native';
 import Main from './src/components/Main.jsx';
 import Logo from './src/components/logo.jsx';
 import Botones from './src/components/botones.jsx';
-import { AUTHENTICATION_API_URL } from '@env'
+import { AUTHENTICATION_API_URL } from '@env';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 console.log(AUTHENTICATION_API_URL)
 
 export default function App() {
   return (
-    <><Main />
-      
-      <Logo />
-      <Botones />
-      
-    
-    </>
+    <NavigationContainer>
+      <Rutas/>
+    </NavigationContainer>
   );
 }
+
+function Rutas() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={Main} />
+
+    </Stack.Navigator>
+  );
+}
+
 
 
 
