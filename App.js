721a1205341e698f5c3afc8b03b2dc23408bf0e2
wilodxd <71090506+wilodxd@users.noreply.github.com>
2023-04-08 18:48:59 -0400

    notification

--- App.js
+++ App.js
@@ -1,39 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View , Image,Button, Alert, Pressable } from 'react-native';
+import { StyleSheet, Text, View , Image,Button, Alert, Pressable, Platform } from 'react-native';
 import Rutas from './src/routes/Rutas.jsx'; // Importamos las rutas
 import { AUTHENTICATION_API_URL } from '@env';
 import {NavigationContainer} from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 
 console.log(AUTHENTICATION_API_URL)
 
 export default function App() {
-  return (
-    // Este contenedor es necesario para navegar entre pantallas
-    <NavigationContainer>
-      {/* LLamamos a las rutas */}
-      <Rutas/>
-    </NavigationContainer>
-  );
+
+    // Guardar el token en el AsyncStorage
+    // const storeFMCToken = async (value) => {
+    //     try {
+    //         await AsyncStorage.setItem('@FMCtoken', value)
+    //     } catch (e) {
+    //         // saving error
+    //         console.log(e)
+    //     }
+    // }
+
+    const requestUserPermission = async () => {
+        const authStatus = await messaging().requestPermission();
+        const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+            console.log('Authorization status:', authStatus);
+        }
+    }
+
+    if (Platform.OS === 'android') {
+
+        useEffect(() => {
+            if( requestUserPermission() ){
+                messaging().getToken().then(token => {
+                    console.log(token)
+                    // storeFMCToken(token)
+                });
+            }else {
+                console.log('No se pudo obtener el token', authStatus);
+            }
+
+            // Check whether an initial notification is available
+            messaging()
+                .getInitialNotification()
+                .then( async (remoteMessage) => {
+                    if (remoteMessage) {
+                    console.log(
+                        'Notification caused app to open from quit state:',
+                        remoteMessage.notification,
+                    );
+                    setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+                    }
+                    //el siguiente codigo 
+                    setLoading(false);
+                }
+            );
+
+            // Register foreground handler
+            messaging().onNotificationOpenedApp(remoteMessage => {
+                console.log(
+                    'Notification caused app to open from background state:',
+                    remoteMessage.notification,
+                );
+            });
+
+            // Register background handler
+            messaging().setBackgroundMessageHandler(async remoteMessage => {            
+                console.log('Message handled in the background!', remoteMessage);
+                try {
+                    // play the file in the file system (sound.mp3)
+                    SoundPlayer.playSoundFile('alarm', 'mp3')
+                        
+                } catch (e) {
+                    console.log(`cannot play the sound file`, e)
+                }
+            });
+
+            // Register foreground handler
+            const unsubscribe = messaging().onMessage(async remoteMessage => {
+                Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+            });
+        
+            return unsubscribe;
+    
+        }, []);
+
+    }
+
+    return (
+        // Este contenedor es necesario para navegar entre pantallas
+        <NavigationContainer>
+        {/* LLamamos a las rutas */}
+        <Rutas/>
+        </NavigationContainer>
+    );
+
 }
-
-
-
-
-
-
-
-
-/*  Como definir estilos
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f2f2',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-
-});
-*/
