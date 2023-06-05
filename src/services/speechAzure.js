d6e8b2b370d26e36c4d87c3d2707cd0b2cba1a78
wilodxd <wilod.hola@gmail.com>
2023-06-05 00:23:14 -0400

    tts fixed

--- src/services/speechAzure.js
+++ src/services/speechAzure.js
@@ -4,40 +4,34 @@ import { Audio } from 'expo-av';
 import { VPS_TTS } from '@env';
 
 /**
- * Guarda el archivo de audio en el dispositivo de forma temporal
- * y lo reproduce con el reproductor de audio del dispositivo
+ * Reproduce el audio de Azure
  * @param {*} text 
  */
 const speechAzure = async (text) => {
     
+    // Verificar
+
+    // Verificar si el texto está vacio
+    if( text.trim() === '' ){
+        return;
+    }
+
+    // Reemplazar los espacios por %20
     text = text.replace(/ /g, "%20");
 
     // Obtener el archivo de audio
     // const url = `http://192.168.1.127:3000/convert?text=${text}`;
     const url = `${VPS_TTS}/convert?text=${text}`;    
     
-    console.log(`Obteniendo el archivo de audio de: ${url}`)
-    const { uri } = await FileSystem.downloadAsync(
-        url,
-        FileSystem.documentDirectory + 'audio.mp3'
+    // Play the sound with Expo's audio API
+    const { sound } = await Audio.Sound.createAsync(
+        { uri: url },
+        { shouldPlay: true }
     );
-    console.log(`Archivo de audio guardado en: ${uri}`)
 
-    // Reproducir el archivo de audio
-    const soundObject = new Audio.Sound();
-    // set audio volume
-    soundObject.setVolumeAsync(1.0);
-
-    try {
-        await soundObject.loadAsync({ uri: uri });
-        await soundObject.playAsync();
-        // Your sound is playing!
-    }
-    catch (error) {
-        // An error occurred!
-        console.log(error)
-    }
-
+    await sound.playAsync();
+    // Your sound is playing!
+    console.log("Reproduciendo el sonido!!!!!!")
 
 }
 
